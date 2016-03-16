44d58ebbdf609b4bf99ff195a1f0681a9aaa2c06
Pieter Scheffers <Pieter Scheffers>
2016-03-16 17:29:53 +0100

    djghsdg

---
+++ README.md
@@ -0,0 +1,3 @@
+- (survivejs - Webpack and react)[http://survivejs.com/webpack_react/webpack_and_react/]
+
+http://survivejs.com/webpack_react/implementing_notes/
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
@@ -18,9 +19,47 @@ const common = {
     entry: {
         app: PATHS.app
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    "presets": [
+                        "stage-0",
+                        "es2015",
+                        "react"
+                    ],
+                    "plugins": [
+                        "lodash"
+                    ],
+                    "env": {
+                        "start": {
+                            "presets": [
+                                "react-hmre"
+                            ]
+                        }
+                    }
+                },
+                include: PATHS.app
+            }
+        ]
     }
 };
 
@@ -29,6 +68,9 @@ const common = {
 if(TARGET === 'start' || !TARGET) {
 
     module.exports = merge(common, {
+
+        devtool: 'eval-source-map',
+
         devServer: {
             contentBase: PATHS.build,
 
